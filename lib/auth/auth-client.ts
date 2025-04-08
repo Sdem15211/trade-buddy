ff9186ea14dc4cc0029cd2ed18a18b36ede6aff0
Simon Demeulemeester <simondemeulemeester9@gmail.com>
2025-04-08 11:00:36 +0200

    changed auth urls to work in prod

--- lib/auth/auth-client.ts
+++ lib/auth/auth-client.ts
@@ -1,7 +1,10 @@
 import { createAuthClient } from "better-auth/react";
 
 export const authClient = createAuthClient({
-  baseURL: "http://localhost:3000",
+  baseURL:
+    process.env.NODE_ENV === "production"
+      ? process.env.NEXT_PUBLIC_BETTER_AUTH_URL
+      : process.env.NEXT_PUBLIC_BETTER_AUTH_DEVELOPMENT_URL,
 });
 
 export const { signIn, signOut, useSession } = authClient;
